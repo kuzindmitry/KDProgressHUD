f1304ef13e4f02242df3f7c7fa01c5958f5d45ec
Dmitry Kuzin <dmitry@kuzin.es>
2017-08-28 21:36:12 +0300

    Initial Commit

---
+++ KDProgressHUD/KDProgressHUD.h
@@ -0,0 +1,19 @@
+//
+//  KDProgressHUD.h
+//  KDProgressHUD
+//
+//  Created by kuzindmitry on 28.08.17.
+//  Copyright © 2017 Dmitry Kuzin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KDProgressHUD.
+FOUNDATION_EXPORT double KDProgressHUDVersionNumber;
+
+//! Project version string for KDProgressHUD.
+FOUNDATION_EXPORT const unsigned char KDProgressHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KDProgressHUD/PublicHeader.h>
+
+
